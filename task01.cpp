9e8b03f471838e28bbc2dd1e2309889f637a1f8f
Jan Turon <janturon@email.cz>
2019-10-17 19:01:25 +0200

    revert back

--- task01.cpp
+++ task01.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
 
-const char* toUpper(const char* input) {
+char* toUpper(char* input) {
 	// TO DO
 }
 
 int main() {
-	const char* input = "Hello, World!";
+	char input[] = "Hello, World!";
 	puts(toUpper(input)); // should print HELLO, WORLD!
 }
