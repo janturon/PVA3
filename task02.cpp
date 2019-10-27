d84e4222dd06c6d1815f18d44ca6f59b26e0f37b
Jan Turon <janturon@email.cz>
2019-10-27 21:17:50 +0100

    comment updated to english

--- task02.cpp
+++ task02.cpp
@@ -1,14 +1,12 @@
 /*
+Decode the task02.dat file and find out the birth dates of John Doe and Sam Sepiol.
+
 format: N records of 6 bytes:
   byte 1-2: birth date yyyyyyyd ddddmmmm (year of birth LSB - 1900)
   byte 3-6: height (float LSB)
 
 separator 0xFF (after N records)
 name1|name2|...|nameN|
-
------
-Na základě těchto údajů o formátu zjistětě z přiloženého souboru task02.dat
-kdy se narodil John Doe a kdy Sam Sepiol
 */
 
 
