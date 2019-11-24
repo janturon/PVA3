15b1b110b7e1de4c90f7cbc35244a37955151c44
Jan Turon <janturon@email.cz>
2019-11-24 17:39:06 +0100

    task03 added

---
+++ task03.cpp
@@ -0,0 +1,17 @@
+/*
+  Write a "task03" program for basic adding. It should handle -f, -i and -v parameters
+
+  task03 -f data.txt // outputs sum of numbers in data.txt
+  task03 -i // interactive mode: sums numbers from standard input until empty input provided
+  task03 4 7 12 // outputs sum of any number of arguments (23 in this case)
+  implement verbose mode -v, which outputs additional info about arguments, i.e.
+    task03 -f data.txt -v // should print "sum of numbers in data.txt is 13"
+    task03 -v -f data.txt // same result
+    task03 -v -i // should print like "sum of 7 numbers from standard input is 38"
+    task03 2 2 13 6 -v // should print "sum of 4 arguments is 23"
+    task03 -v 13 4 // should print "sum of 2 arguments is 17"
+*/
+
+int main(int argc, char* argv[]) {
+  // TODO
+}
