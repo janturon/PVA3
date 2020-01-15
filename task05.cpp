49591532386969c2db8791e29164d796223323f2
Jan Turon <janturon@email.cz>
2020-01-15 09:06:57 +0100

    task05 added

---
+++ task05.cpp
@@ -0,0 +1,5 @@
+/*
+  Napište abstraktní třídu Button s ryze virtuálními metodami waitForButton (která bude čekat na stisk tlačítka) a hello (která vytiskne hello při každém stisku tlačítka).
+  Napište její hardwarovou implementaci pro PC jménem ButtonPC (waitForButton bude čekat na stisk enter a hello bude vypisovat na konzoli).
+  Dobře navržený kód umožní použití třídy Button na Arduinu (waitForButton bude čekat na stisk připojeného tlačítka a hello bude vypisovat na Sériovou linku). Tuto část si demonstrujeme spolu při hodině.
+*/
